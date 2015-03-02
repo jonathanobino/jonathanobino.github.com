5f8966f2309610294ee9b48103dc9fb36c048b2f
Jonathan Obino <jonobin@gmail.com>
2015-03-02 17:39:41 +0100

    location path fix

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@
 
 	})
 
-	.controller('ConnectionData', function($scope,$firebase){
+	.controller('ConnectionData', function($scope,$firebase,$location){
 		var url="https://radiant-fire-9839.firebaseio.com/aziende";
 		var ref= new Firebase(url);
 		 $scope.notfound=false;
@@ -35,7 +35,7 @@
 		 	$scope.newCompany.recensioni=[];
 		 	ref.push($scope.newCompany);
 		 	$scope.newCompany={};
-		 	$window.location.href = "#/";
+		 	$location.path('/#')
 		 }
 		 
 		
