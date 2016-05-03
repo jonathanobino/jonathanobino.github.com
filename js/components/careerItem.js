6b3af3c5fefb2d8c273db9263aed38ea233eb992
Jonathan Obino <Jonathan@MacBook-Pro-di-Jonathan.local>
2016-05-03 13:28:35 +0200

    what I use section rewritten with flexbox

--- js/components/careerItem.js
+++ js/components/careerItem.js
@@ -1,25 +1,23 @@
 import React from 'react';
 
-const PortfolioItem = React.createClass({
-	render: function(){
-		let image = this.props.item.imageSrc ? <div className="imgWrapper" style={{backgroundImage:`url(${this.props.item.imageSrc})`}}>
+const PortfolioItem = ({item}) => {
+		let image = item.imageSrc ? <div className="imgWrapper" style={{backgroundImage:`url(${item.imageSrc})`}}>
 							</div> : <div className="imgWrapper hide-sm"></div>
-		let technology  = this.props.item.description.technology ? <p><span className="sans">Technology Involved</span> {this.props.item.description.technology}</p>:null;
+		let technology  = item.description.technology ? <p><span className="sans">Technology Involved</span> {item.description.technology}</p>:null;
 		return (
 				<div className="row careerItem">
 					<div className="large-9 medium-8 small-9 columns description">
-						<p><span className="sans">Company</span> <strong>{this.props.item.description.company}</strong></p>
-						<p><span className="sans">Role</span> {this.props.item.description.role}</p>
-						<p><span className="sans">When</span> {this.props.item.description.when}</p>
+						<p><span className="sans">Company</span> <strong>{item.description.company}</strong></p>
+						<p><span className="sans">Role</span> {item.description.role}</p>
+						<p><span className="sans">When</span> {item.description.when}</p>
 					</div>
 					<div className="large-3 medium-4 small-3 columns">
-						<a href={this.props.item.link} target="_blank">
+						<a href={item.link} target="_blank">
 							{image}
 						</a>
 					</div>
 				</div>
 			)
 	}
-})
 
 export default PortfolioItem;
