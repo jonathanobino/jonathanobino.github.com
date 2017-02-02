c54e7a264904277c1abe7ba123ab6bcd28a244c2
Jonathan <jonobin@gmail.com>
2017-02-02 17:54:24 +0100

    Update to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -9,16 +9,14 @@ module.exports = {
     filename: 'build.js'
   },
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
-      loader: 'babel',
+      use: 'babel-loader',
       exclude: /node_modules/
     }]
   },
    plugins: [
     new webpack.optimize.UglifyJsPlugin({minimize: true}),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     })
