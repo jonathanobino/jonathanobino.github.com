cacdd698ee5843c022970372876b3821d3cfaeba
Jonathan Obino <Jonathan@mbp-di-jonathan.homenet.telecomitalia.it>
2017-03-15 15:04:14 +0100

    split code with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry:'./js/main.js' ,
+  entry: {
+    app:'./js/main.js',
+    vendor: ['react', 'react-dom','axios','velocity-animate']
+  },
   devtool: 'cheap-module-source-map',
   output: {
     path: path.join(__dirname),
-    filename: 'build.js'
+    filename: '[name].[hash].js'
   },
   module: {
     rules: [{
@@ -20,8 +25,22 @@ module.exports = {
       minimize: true,
       comments: false
     }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: '[name].[hash].js',
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './rawIndex.html'),
+      filename: 'index.html'
+    }),
+    new PreloadWebpackPlugin({
+      rel: 'preload',
+      as: 'script',
+      include: 'all'
     })
   ]
 };
