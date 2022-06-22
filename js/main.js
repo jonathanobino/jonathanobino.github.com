ca4863393fd6c3ea6039fea7b92825a750c310c3
Jonathan Obino <jonathanobino@gmail.com>
2022-06-22 19:49:21 +0200

    ➖ remove gulp dependencies, move to scss-loader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import Home from './pages/home';
+import '../scss/style.scss';
 
 render(<Home />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,14 @@ module.exports = {
       test: /\.js$/,
       use: 'babel-loader',
       exclude: /node_modules/
-    }]
+    }, {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },]
   },
   optimization:{
     minimize:true,
