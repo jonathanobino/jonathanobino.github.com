6ab0cda1a9c22c2c478c4f1ae77eb5540f3907db
Jonathan Obino <jonathanobino@gmail.com>
2022-07-01 17:20:33 +0200

    ✨ move to hooks

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,166 +1,106 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { useState, useRef } from 'react';
+import useFetch from '../API/useFetch';
+import { emailValidator } from '../utils';
 
-class ContactForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isSending: false,
-      successfulSended: false,
-      errorSending: false,
-      btnDisabled: true,
-      errorMessage: '',
-    };
-    this.sendEmail = this.sendEmail.bind(this);
-    this.emailValidator = this.emailValidator.bind(this);
-    this.handleCanSend = this.handleCanSend.bind(this);
-    this.checkBeforeSend = this.checkBeforeSend.bind(this);
-  }
+export default function () {
+  const [result, isSending, error, send, updateOptions] = useFetch(
+    'https://formspree.io/f/xyyojjyj',
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    },
+    true
+  );
+  const formRef = useRef(null);
+  const [formMessage, setFormMessage] = useState({
+    message: "Let's get in touch!",
+    isError: false,
+  });
 
-  sendEmail() {
-    if (this.checkBeforeSend()) {
-      this.setState({ isSending: true });
-      axios({
-        method: 'POST',
-        url: `https://formspree.io/f/xyyojjyj`,
-        data: {
-          name: this.refs.name.value,
-          email: this.refs.email.value,
-          message: this.refs.text.value,
-        },
-        headers: {
-          Accept: 'application/json',
-        },
-      })
-        .then((res) => {
-          if (res.data.ok) {
-            this.setState({
-              successfulSended: true,
-              isSending: false,
-            });
-          } else {
-            this.setState({
-              errorSending: true,
-              isSending: false,
-            });
-          }
-        })
-        .catch((err) => {
-          this.setState({
-            errorSending: true,
-            isSending: false,
-          });
-        });
-    }
-  }
-
-  emailValidator(value) {
-    return /[(a-zA-Z0-9)]+[@]+[(a-zA-Z0-9)]+\.+[(a-zA-Z0-9)]+$/gi.test(value);
-  }
-
-  handleCanSend(event) {
-    if (!this.emailValidator(this.refs.email.value))
-      this.setState({ errorMessage: 'There must be a valid email here!' });
-    else if (this.refs.text.value.split('').length == 0)
-      this.setState({ errorMessage: 'There must be some text here!' });
-    else this.setState({ errorMessage: '' });
-  }
-
-  checkBeforeSend() {
+  function validateForm(data) {
     return (
-      this.refs.text.value.split('').length > 0 &&
-      this.emailValidator(this.refs.email.value)
+      data.name !== '' &&
+      data.email !== '' &&
+      emailValidator(data.email) &&
+      data.message !== ''
+
     );
   }
 
-  render() {
-    let message;
-    let cantSend =
-      this.state.errorMessage !== '' ? (
-        <h2 className="error">{this.state.errorMessage}</h2>
-      ) : (
-        <h2>Let's get in touch!</h2>
-      );
-    if (this.state.isSending) {
-      message = (
-        <img src="http://samherbert.net/svg-loaders/svg-loaders/three-dots.svg" />
-      );
-    } else if (this.state.successfulSended) {
-      message = (
-        <h2>
-          {' '}
-          Thank you! I'll be in touch shortly!{' '}
-          <i className="fa fa-thumbs-o-up"></i>
-        </h2>
-      );
-    } else if (this.state.errorSending) {
-      message = (
-        <h2>
-          {' '}
-          Hey! An error has occoured! Please try to refresh the page and send it
-          again.
-        </h2>
-      );
+  function beforeSend(data) {
+    if (!validateForm(data))
+      setFormMessage({
+        message: 'Fill every field before sending',
+        isError: true,
+      });
+    else {
+      updateOptions({ body: JSON.stringify(data) });
+      setFormMessage({
+        message: "Thank you! I'll be in touch shortly!",
+        isError: false,
+      });
+      send();
     }
+  }
 
-    return (
-      <div className="contactFormWrapper">
-        <div
-          className={
-            'message ' +
-            (this.state.successfulSended === true
-              ? 'show'
-              : this.state.errorSending == true
-              ? 'show'
-              : this.state.isSending == true
-              ? 'show'
-              : '')
-          }
-        >
-          {message}
-        </div>
-        <div className="contactForm">
-          <div className="row">
-            <div className="medium-12 columns medium-centered text-center">
-              {cantSend}
-            </div>
-          </div>
-          <div className="row">
-            <div className="medium-6 columns">
-              <input type="text" ref="name" placeholder="Name" />
-            </div>
-            <div className="medium-6 columns">
-              <input
-                type="email"
-                ref="email"
-                placeholder="Email"
-                onChange={this.handleCanSend}
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="medium-12 columns">
-              <textarea
-                ref="text"
-                placeholder="Message"
-                rows="5"
-                onChange={this.handleCanSend}
-              />
-            </div>
-            <div className="medium-12 columns">
-              <button
-                className="button sendForm"
-                onClick={this.sendEmail}
-                ref="send"
-              >
-                Send
-              </button>
-            </div>
-          </div>
-        </div>
+  return (
+    <div className="contactFormWrapper">
+      <div className={`message ${isSending || result || error ? 'show' : ''}`}>
+        {isSending && (
+          <img src="http://samherbert.net/svg-loaders/svg-loaders/three-dots.svg" />
+        )}
+        {/*message error from api*/}
+        {!isSending && result && (
+          <h2>
+            {' '}
+            Thank you! I'll be in touch shortly!{' '}
+            <i className="fa fa-thumbs-o-up"></i>
+          </h2>
+        )}
+        {!isSending && error && (
+          <h2>
+            {' '}
+            Hey! An error has occoured! Please try to refresh the page and send
+            it again.
+          </h2>
+        )}
       </div>
-    );
-  }
-}
 
-export default ContactForm;
+      <form
+        className="contactForm"
+        ref={formRef}
+        onSubmit={(e) => {
+          e.preventDefault();
+          beforeSend(Object.fromEntries(new FormData(e.target)));
+        }}
+      >
+        <div className="row">
+          <div className="medium-12 columns medium-centered text-center">
+            <h2 className={formMessage.isError ? 'error' : ''}>
+              {formMessage.message}
+            </h2>
+            {/*cantSend form validation*/}
+          </div>
+        </div>
+        <div className="row">
+          <div className="medium-6 columns">
+            <input type="text" placeholder="Name" name="name" />
+          </div>
+          <div className="medium-6 columns">
+            <input type="email" placeholder="Email" name="email" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="medium-12 columns">
+            <textarea placeholder="Message" name="message" rows="5" />
+          </div>
+          <div className="medium-12 columns">
+            <input type="submit" className=" button sendForm" value="Send" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
