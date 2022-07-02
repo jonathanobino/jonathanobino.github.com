c3900538e6ad07d5f7cd179e852706380eb0a9af
Jonathan Obino <jonathanobino@gmail.com>
2022-07-02 13:03:48 +0200

    ⚡️ move openSource component to hooks

---
+++ src/components/openSource.js
@@ -0,0 +1,49 @@
+import React, { Component, useState, useEffect } from 'react'
+import useIsInViewport from 'lazy-react'
+import Loading from '../components/Loading';
+import GitHub from '../components/githubRepo';
+
+const projects = [
+  {
+    endpoint: 'https://api.github.com/repos/jonathanobino/react-lazy',
+  },
+  {
+    endpoint: 'https://api.github.com/repos/jonathanobino/rately',
+  },
+]
+
+export default function OpenSource() {
+
+  const [results, setResults] = useState([]);
+
+  const [setRef, link, isVisible] = useIsInViewport({link:'', offset: 200})
+
+  useEffect(()=>{
+    if(isVisible){
+      Promise.all(
+      projects.map((elem) => {
+        return fetch(elem.endpoint).then(res => res.json())
+      })
+    ).then((values) => {
+      setResults(values);
+    });
+    }
+  }, [isVisible]);
+
+  return (
+    <div className="row main" ref={(node) => setRef(node)}>
+      <div className="medium-2 large-2 columns text-right">
+        <h2>Open Source Projects</h2>
+      </div>
+      <div className="medium-10 large-10 columns spaceBetween">
+        {results.length === 0 ? (
+          <Loading />
+        ) : (
+          results.map((elem, index) => (
+            <GitHub {...elem} key={index} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
