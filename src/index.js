d20a3921e565a4ebbad4d42022827f4b0ae7e28e
Jonathan Obino <jonathanobino@gmail.com>
2025-03-31 23:52:04 +0200

    build: ⬆️ update react, update lazy-react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import Home from './pages/home';
 import './scss/style.scss';
 
-render(<Home />, document.getElementById('app'));
+const container = document.getElementById('app');
+
+const root = createRoot(container);
+root.render(<Home />)
+
+
