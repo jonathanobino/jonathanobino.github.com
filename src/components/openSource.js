076f87dcbc85960d4fe57b9ecd5fd1cd35c9a7d4
Jonathan Obino <jonathanobino@gmail.com>
2026-01-16 15:45:10 +0100

    fix: 🔥 remove old repo

--- src/components/openSource.js
+++ src/components/openSource.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import useIsInViewport from 'lazy-react';
-import Loading from '../components/Loading';
-import GitHub from '../components/githubRepo';
+import React, { useState, useEffect } from "react";
+import useIsInViewport from "lazy-react";
+import Loading from "../components/Loading";
+import GitHub from "../components/githubRepo";
 
 const projects = [
   {
-    endpoint: 'https://api.github.com/repos/jonathanobino/react-lazy',
+    endpoint: "https://api.github.com/repos/jonathanobino/react-lazy",
   },
-  {
-    endpoint: 'https://api.github.com/repos/jonathanobino/rately',
-  },
+  // {
+  //   endpoint: 'https://api.github.com/repos/jonathanobino/rately',
+  // },
 ];
 
 export default function OpenSource() {
